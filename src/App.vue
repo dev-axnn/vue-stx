5ccfad0db8bf6b1064e2486a591ad5714ddfdc6b
dev-axnn <dev.axnn@gmail.com>
2022-08-23 10:55:29 +0900

    vue 와 git 연동

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,251 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Modal></Modal>
+
+    <div class="wrap">
+      <Header :gnbdata="gnbData"></Header>
+      <Visual></Visual>
+      <WorkPart></WorkPart>
+      <News :noticedata="noticeData" :newsdata="newsData"></News>
+      <CS></CS>
+      <About></About>
+      <Footer></Footer>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Modal from './components/ModalPop.vue';
+  import Header from './components/HeaderView.vue';
+  import Visual from './components/VisualView.vue';
+  import WorkPart from './components/WorkPartView.vue';
+  import News from './components/NewsView.vue';
+  import CS from './components/CSView.vue';
+  import About from './components/AboutView.vue';
+  import Footer from './components/FooterView.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: "App",
+    components: {
+      Modal,
+      Header,
+      Visual,
+      WorkPart,
+      News,
+      CS,
+      About,
+      Footer
+    },
+    
+    setup() {
+      const gnbData = [
+        {
+          mainurl: '#', 
+          maintxt: '회사소개',
+          subData: [
+            {
+              suburl: '#',
+              subtxt: '인사말'
+            },
+            {
+              suburl: '#',
+              subtxt: 'STX건설'
+            },
+            {
+              suburl: '#',
+              subtxt: '기업문화'
+            },
+            {
+              suburl: '#',
+              subtxt: 'STX건설연혁'
+            },
+            {
+              suburl: '#',
+              subtxt: '조직안내'
+            },
+            {
+              suburl: '#',
+              subtxt: '윤리경영'
+            },
+            {
+              suburl: '#',
+              subtxt: '안전·환경·품질경영'
+            },
+            {
+              suburl: '#',
+              subtxt: '찾아오시는길'
+            }
+          ]
+        },
+        {
+          mainurl: '#', 
+          maintxt: '사업분야',
+          subData: [
+            {
+              suburl: '#',
+              subtxt: '건축사업'
+            },
+            {
+              suburl: '#',
+              subtxt: '주택사업'
+            },
+            {
+              suburl: '#',
+              subtxt: '토목사업'
+            },
+            {
+              suburl: '#',
+              subtxt: '플랜트/공작기계사업'
+            },
+            {
+              suburl: '#',
+              subtxt: '해외사업'
+            }
+          ]
+        },
+        {
+          mainurl: '#', 
+          maintxt: '사회공헌',
+          subData: [
+            {
+              suburl: '#',
+              subtxt: '나눔의생각'
+            },
+            {
+              suburl: '#',
+              subtxt: '주요활동분야'
+            },
+            {
+              suburl: '#',
+              subtxt: '활동현황'
+            }
+          ]
+        },
+        {
+          mainurl: '#', 
+          maintxt: '홍보센터',
+          subData: [
+            {
+              suburl: '#',
+              subtxt: '홍보동영상'
+            },
+            {
+              suburl: '#',
+              subtxt: '홍보브로슈어'
+            },
+            {
+              suburl: '#',
+              subtxt: 'STX건설뉴스'
+            }
+          ]
+        },
+        {
+          mainurl: '#', 
+          maintxt: '고객지원',
+          subData: [
+            {
+              suburl: '#',
+              subtxt: '자주묻는질문'
+            },
+            {
+              suburl: '#',
+              subtxt: '고객문의'
+            }
+          ]
+        },
+        {
+          mainurl: '#', 
+          maintxt: '채용정보',
+          subData: [
+            {
+              suburl: '#',
+              subtxt: '채용안내'
+            },
+            {
+              suburl: '#',
+              subtxt: '채용공고'
+            },
+            {
+              suburl: '#',
+              subtxt: '채용FAQ'
+            }
+          ]
+        }
+      ];
+      const noticeData = [
+        {url: 'a.html', title:'에스티엑스건설자산관리 주식회사 해산결의에 따른 채권신고 안내 공고 (2차)'},
+        {url: 'b.html', title:'에스티엑스건설자산관리 주식회사 해산결의에 따른 채권신고 안내 공고 (1차)'},
+        {url: 'c.html', title:'2021년도 협력업체 모집공고'},
+        {url: 'd.html', title:'STX건설 상호 사용 관련 안내'}
+      ];
+      const newsData = [
+        {url: 'k.html', title:'STX건설, 춘천 레고랜드 테마파크 시공사 ‘선정’'},
+        {url: 'i.html', title:'STX건설, 2018년 성장 청신호'}
+      ];
+      
+      return {
+        noticeData,
+        newsData,
+        gnbData
+      };
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @charset "utf-8";
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ul, li {
+    list-style: none;
+  }
+
+  a {
+    color: #333;
+    text-decoration: none;
+  }
+
+  img {
+    vertical-align: middle;
+    border: 0;
+  }
+
+  html {
+    font-size: 16px;
+  }
+
+  body {
+    font-family: 'Noto Sans KR', Helvetica, '맑은 고딕', 'malgun gothic', 'Apple SD Gothic Neo', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1.4;
+    letter-spacing: -0.64px;
+    color: #444;
+  }
+
+  /* === 공통요소 === */
+  .clearfix::after {
+    content: '';
+    position: relative;
+    display: block;
+    width: 100%;
+    clear: both;
+  }
+
+  .inner {
+    position: relative;
+    display: block;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  .wrap {
+    padding-top: 80px;
+  }
 </style>
---
+++ src/components/VisualView.vue
@@ -0,0 +1,90 @@
+<template>
+  <section class="visual">
+    <div class="inner">
+      <h2 class="visual-title">BEST OF THE WORLD<br>STX Engineering & Construction</h2>
+      <P class="visual-txt">세계최고를 향해 STX건설이 힘차게 질주합니다!</P>
+      <button class="visual-bt">아래로 이동</button>
+    </div>
+  </section>
+</template>
+
+<script>
+  import $ from 'jquery';
+  import { onMounted } from 'vue';
+  
+  export default {
+    setup() {
+      onMounted( () => {
+        // 컨텐츠 이동 버튼
+        let visualBt = $('.visual-bt');
+        // .part 영역이 
+        // html 의 상단(top)으로 부터 
+        // 어느만큼 떨어진 위치 px 값
+        let partY = $('.part').offset().top;
+        visualBt.click(function () {
+          $('html').stop().animate({
+            scrollTop: partY 
+          }, 500);
+        });
+      });
+
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+/* visual */
+.visual {
+  background: url('../assets/images/img_visual01.png') no-repeat center;
+  background-size: cover;
+  background-attachment: fixed;  /* 배경 이미지 고정 */
+}
+
+.visual .inner {
+  height: calc(100vh - 80px);
+  border-top: 1px solid rgba(0, 0, 0, 0);
+}
+
+.visual-title {
+  position: relative;
+  font-size: 70px;
+  font-weight: 500;
+  line-height: 1.1;
+  color: #fff;
+  margin-top: 235px;
+}
+
+.visual-title::before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: -50px;
+  width: 60px;
+  height: 5px;
+  background: #ed1c24;
+}
+
+.visual-txt {
+  font-size: 21px;
+  font-weight: 300;
+  color: #fff;
+  margin-top: 40px;
+}
+
+.visual-bt {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 40px;
+  width: 33px;
+  height: 33px;
+  background: url('../assets/images/bg_common.png') no-repeat;
+  background-position: -122px 0px;
+  border: 0;
+  font-size: 0;
+  cursor: pointer;
+}
+</style>
